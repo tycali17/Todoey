4d6d9b47776dc0d5e8dad19a729cd0e546bbd689
Ty Cali <tycali@tys-macbook-pro.domain>
2018-06-11 08:11:43 -0700

    skeleton for category core data set up. attempting successful persistent data storage

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Ty Cali on 6/11/18.
+//  Copyright © 2018 Ty Cali. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    
+    //MARK: - Add New Categories
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    //MARK: - TableView Delegate Methods
+    //what should happen when we click on one of the cells inside the category table view. leave for now
+    
+}
--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
 
     //turned item array into an array of (class->) Action objects.
     var itemArray = [Action]()
     
-    //creates path to document folder. object that provides an interface to the file system
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Actions.plist")
+    var searchBar = UISearchBar()
+    
+    //calls context from app delegate class with singleton
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     //too complex for user defaults
 //    //data stored inside container with user defaults. new object:
@@ -23,9 +26,9 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath!)
+        //views path to document folder. object that provides an interface to the file system
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-        //no need for hard coding appending actions:
         loadActions()
         
         //updates array to match saved plist on loadup; if let keeps app from crashing if plist doesn't exist. change force unwrap as! to optional as?
@@ -35,7 +38,7 @@ class ToDoListViewController: UITableViewController {
        
     }
 
-    //MARK - Tableview Datasource Methods
+    //MARK: - Tableview Datasource Methods
     
     //numberofrows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -64,26 +67,30 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-    //MARK - TableView Delegate Methods
+    //MARK: - TableView Delegate Methods
     
     //indexPath is a local variable set for the selected row
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         //instead of long-ended if/else, just say it's the opposite of itself.
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        self.saveActions()
+        saveActions()
         
         //tap flashes gray instead of staying gray
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     
+    
 //    //Set the tapGesture
 //    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
 //    tableView.addGestureRecognizer(tapGesture)
     
-//MARK  - Add New Items
+    //MARK:  - Add New Actions
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
        
@@ -97,8 +104,12 @@ class ToDoListViewController: UITableViewController {
         //how you add items to list! -an action taken when user presses button in an alert
         let action = UIAlertAction(title: "Add Action", style: .default) { (action) in
             //what will happen once the user clicks the Add Action button on our UIAlert. Must be done like this for class object
-            let newAction = Action()
+            
+            
+            //class (Action) automatically gets generated with entity title Action inside data model. That class already has access to properties specified, i.e., title/done
+            let newAction = Action(context: self.context)
             newAction.title = textField.text!
+            newAction.done = false //every new item starts off unchecked/not done/false.
             
             //add to end of array Action object. needs self because it's a closure.
             self.itemArray.append(newAction)
@@ -125,30 +136,65 @@ class ToDoListViewController: UITableViewController {
     
     //encode method
     func saveActions() {
-        //saves updated item array to user defaults (in plist), still needs to load up table view
-        let encoder = PropertyListEncoder()
+
+        //sets up code to use Core Data for saving new items that have been added using the UIAlert.
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         //the magic method. reloads rows & sections of array, taking into account newly added items
         self.tableView.reloadData()
     }
-    
-    //decode method
-    func loadActions() {
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Action].self, from: data)
-            } catch {
-                print("Error decoding item array, \(error)")
-            }
+    //decode method. with is external usage; request: is internal usage
+    func loadActions(with request: NSFetchRequest<Action> = Action.fetchRequest()) {
+        //deleted- let request : NSFetchRequest<Action> = Action.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+            
         }
+        tableView.reloadData()
     }
     
-    
 }
-
+//MARK: searchbar methods
+//modularizes class into categories.
+//optional: extension for lengthy class delegate list:
+extension ToDoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+       
+        //Dismiss keyboard
+        DispatchQueue.main.async {
+            searchBar.resignFirstResponder()
+        }
+//        do {
+//            itemArray = try context.fetch(request)
+//        } catch {
+//            print("Error fetching data from context \(error)")
+//        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadActions()
+        
+            //Run on the main thread so that searchbar loses focus and keyboard is dimissed, even if background threads are running. Otherwise, app might assign this code to another thread...
+            DispatchQueue.main.async {
+            searchBar.resignFirstResponder()
+            }
+        }
+        else if searchBar.text!.count > 0 {
+            let request : NSFetchRequest<Action> = Action.fetchRequest()
+            
+            //what is going to be our filter/query?
+            request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+            
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+            loadActions(with: request)
+        }
+        
+    }
+}
